ae725661104d39b873ff65d49e0f0ba0342a24f7
Murtada <43283994+Mortida@users.noreply.github.com>
2021-06-17 15:09:22 +0100

    added links

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,14 +1,34 @@
 /*===== MENU SHOW =====*/
-const shoeMenu = (toggleId, navId, navI) => {
+const showMenu = (toggleId, navId, navI, act) => {
 	const toggle = document.getElementById(toggleId),
 		nav = document.getElementById(navId),
+		active = document.querySelector(act),
 		navIcon = document.getElementById(navI);
+	
 
 	if (toggle && nav) {
 		toggle.addEventListener('click', (e) => {
 			nav.classList.toggle('show');
 			navIcon.classList.toggle('toggle__Icon');
 		});
+	};
+
+	for(let index of active.children)
+	{
+		index.addEventListener('click', (e) => {
+			e.target.classList.toggle('active');
+			let pre = index.previousElementSibling;
+			if(pre)
+			{
+				sibiling = e.target.parentElement.previousElementSibling.children[0];
+				sibiling.classList.remove('active')
+				// 
+			} else if (active.firstElementChild.classList.contains('active')) {
+				console.log(active.lastElementChild);
+			}
+
+		});
 	}
+
 };
-shoeMenu('nav-toggle', 'nav-menu', 'nav-toggle-icon');
+showMenu('nav-toggle', 'nav-menu', 'nav-toggle-icon', '.nav__list');
